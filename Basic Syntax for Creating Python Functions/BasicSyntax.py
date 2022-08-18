dc568d1979af49830d337ed462dbdac9756ddcb3
BeardR6S <brandon.allan.hale@gmail.com>
2022-08-18 13:36:12 -0400

    first commit

---
+++ Basic Syntax for Creating Python Functions/BasicSyntax.py
@@ -0,0 +1,29 @@
+# def full_name(first, last):
+#     print(f'{first} {last}')
+
+# full_name('Kristine', 'Hudgens')
+
+#-----------------------------------------------------------------
+
+# def auth(email, password):
+#     if email == 'kristine@hudgens.com' and password == 'secret':
+#         print('Authorized')
+#     else:
+#         print('Not authorized')
+
+
+# auth('jordan@hudgens.com', 'secret')
+
+#-----------------------------------------------------------------
+
+# def hundred():
+#     for num in range(1, 101):
+#         print(num)
+
+# hundred()
+
+def counter(max_value):
+    for num in range(1, max_value):
+        print(num)
+
+counter(501)
